0b9714a5911a5b4388e06c271d4bbae2e3aa1348
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-03-07 19:04:12 +0000

    Done work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
+
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
